52cbcd98c073e23a5538ce32c4115576a8be32a8
cultureweb <cedric.milius@yahoo.fr>
2020-12-02 21:04:16 +0100

    modify script 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import HomeContainer from './views/Home/HomeContainer';
 import About from "./views/About/About.js";
