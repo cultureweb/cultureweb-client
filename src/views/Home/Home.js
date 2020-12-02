598697499f73854a209626c77a884d0bc4f0babd
cultureweb <cedric.milius@yahoo.fr>
2020-12-02 20:50:47 +0100

    deploy test

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -5,6 +5,7 @@ export const Home = ({ datas }) => {
     return(
     <>
        <h1>Welcome Home</h1> 
+       <p>test deploy</p>
             {datas.map(post => 
                 <p key={post.title}> {post.title} </p> 
             )
